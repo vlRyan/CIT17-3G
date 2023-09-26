48db2d1e4e53dd5de33589ebbbfd210ced86ab71
Ryan Pul-ocan <ryan.pulocan6@gmail.com>
2023-09-26 23:30:21 +0800

    first commit

---
+++ phpTutorial01.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Basic Sytax</title>
+</head>
+<body>
+  <p>PHP Here we goooo!!!!</p>
+  <?php 
+  #hello 
+  //world
+  /*
+    awooooooooooo
+    awooooooooo
+  */
+  $boolean = true;
+  $num = 9;
+  
+  print($boolean);
+  print($num);
+
+  if (true) {
+    
+    print(23231232);
+  }
+
+  echo "What is different with print";
+
+  $add = $num + 1;
+  echo $add;
+  ?>
+</body>
+</html>
+
