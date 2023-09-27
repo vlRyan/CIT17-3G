034086bf894ba060e8b4a810105c27811ff3edfa
Ryan <ryan.pulocan6@gmail.com>
2023-09-27 18:35:07 +0800

    added lessons

--- 04_variables.php
+++ 04_variables.php
@@ -3,11 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Variable Types</title>
 </head>
 <body>
     <?php 
-    
+        $addNum = 2 + 2;
+        echo "$addNum";
+        
+        $four =
+        2+
+        2; 
+
+        print ($four);
+
+        $capital = 67;
+        print("Variable capital is $capital<br>");
+
+        $jason = "chopapi";
+
+        echo "<br>$jason";
+        
+        $weight = 90.35;
+        $height = 173
     ?>
 </body>
 </html>
