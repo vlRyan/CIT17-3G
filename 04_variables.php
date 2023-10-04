ba62ec4b40855049535847bb265c13827964effc
Ryan <ryan.pulocan6@gmail.com>
2023-10-04 18:25:24 +0800

    continued lessons

--- 04_variables.php
+++ 04_variables.php
@@ -24,7 +24,51 @@
         echo "<br>$jason";
         
         $weight = 90.35;
-        $height = 173
+        $height = 173;
+
+        if (TRUE) {
+            print("This will always print");
+        } else {
+            print("This will never print");
+        };
+            
+        $my_var = NULL;
+        $true_array[49] = "An array element";
+
+        $string_0 = "";
+
+        echo "$string_0";
+
+        $variable = "name";
+        $literally = 'My $variable will not print!\\n';
+        print($literally);
+        $literally = "My $variable will print!\\n";
+        print($literally);
+
+        $somevar = 15;
+        function addit() {
+            GLOBAL $somevar;
+            $somevar++;
+            print "<br>Somevar is $somevar";
+        }
+        addit();
+
+        function multiply ($value) {
+            $value = $value * 10;
+            return $value;
+        }
+        $retval = multiply (10);
+        Print "<br>Return value is $retval\n";
+
+        function keep_track() {
+            STATIC $count = 0;
+            $count++;
+            print $count;
+            print " ";
+        }
+        keep_track();
+        keep_track();
+        keep_track();
     ?>
 </body>
 </html>
